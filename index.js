8f066aa678f9e9f96772d4d683336393854ab7d8
Vrinda Sharma <vrinda.sharma@rtctek.global>
2025-01-22 16:58:09 +0530

    fix

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-const app = express();
+                                     
+                                    const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World 3');
-});
+                             res.send('Hello World 3');
+}
+
+
+
+
+
+);
 
 
 
