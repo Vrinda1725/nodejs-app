af10bc084b06820bda04f1a5c342ddd3c031e8d1
Vrinda Sharma <vrinda.sharma@rtctek.global>
2025-01-22 17:48:15 +0530

    l

--- index.js
+++ index.js
@@ -7,15 +7,10 @@ const HOST = '0.0.0.0';
 
 const app = express();
 
-                                    app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World 3');
 });
 
 app.listen(PORT, HOST, () => {
-
-
-
-
-
-                                console.log(`Running on http://${HOST}:${PORT}`);
+  console.log(`Running on http://${HOST}:${PORT}`);
 });
