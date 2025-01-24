a911fb680d43e6d94c89982404802aefc48f1509
Vrinda Sharma <vrinda.sharma@rtctek.global>
2025-01-24 10:08:16 +0530

    k

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, HOST, () => {
+                                        console.log(`Running on http://${HOST}:${PORT}`);
   console.log(`Running on http://${HOST}:${PORT}`)
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
+}
+
+
+
+);
